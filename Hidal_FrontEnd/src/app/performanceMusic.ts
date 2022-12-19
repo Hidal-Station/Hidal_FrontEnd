7567710f5a82f68330ce09a0d47fa5b723700fff
uyvaquoc123 <hieuzzai9@gmail.com>
2022-12-19 16:02:49 +0700

    feat admin page

---
+++ Hidal_FrontEnd/src/app/performanceMusic.ts
@@ -0,0 +1,17 @@
+export interface PerformanceMusic {
+  id: string;
+  singerName: string;
+  songName: string;
+  url: string;
+}
+
+export const PerformanceMusics = [
+  {
+    id: '7ed3c561-143c-4d50-92f1-8d5c686d93a8',
+    singerName: 'TommyTeo',
+    songName: 'AlabaTrap',
+    url: '/UploadFile/host/music/AlabaTrap-TommyTeo-7ed3c561-143c-4d50-92f1-8d5c686d93a8.mp3',
+  },
+];
+
+
--- Hidal_FrontEnd/src/app/song-list/song-list.component.ts
+++ Hidal_FrontEnd/src/app/song-list/song-list.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PerformMucsicService } from '../perform-mucsic.service';
+import { PerformanceMusics } from '../performanceMusic';
 
 @Component({
   selector: 'app-song-list',
   templateUrl: './song-list.component.html',
   styleUrls: ['./song-list.component.css']
 })
-export class SongListComponent implements OnInit {
-
-  constructor() { }
-
+export class SongListComponent {
+  
   ngOnInit(): void {
+    let link = "https://7199-2402-800-6294-ccf2-8954-de15-c0f4-fa21.ap.ngrok.io";
+    this.PerformanceService.fetch();
+    this.performanceSongs.forEach(element => {
+      element.url = link + element.url;
+    });
+  }
+  SONGS = PerformanceMusics;
+
+  
+  
+  performanceSongs = this.PerformanceService.getAllItems();
+
+  constructor(private PerformanceService: PerformMucsicService) { }
+
+
+  delete(item: any){
+    this.PerformanceService.deleteItemsById(item);
+    window.alert("Deleted!");
   }
 
+  fakeUpload(){
+    this.PerformanceService.fakeUpload();
+    window.alert("fake uploaded");
+  }
 }
